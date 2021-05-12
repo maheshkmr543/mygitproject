bf83f13dfb7eeeb63b6f7f6687ab3f4648bb16fe
mahes <mahes@DESKTOP-4MD939I>
2021-05-12 20:34:40 +0530

    Chrome commit

--- MavenProject/src/main/java/tests/ChromeLaunch.java
+++ MavenProject/src/main/java/tests/ChromeLaunch.java
@@ -1,13 +1,13 @@
 package tests;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class ChromeLaunch {
 	@Test
 	  public void GoogleLaunch() {
-		WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
 		System.out.println("Hello! Welcome to Facebook login page");
 	  }
